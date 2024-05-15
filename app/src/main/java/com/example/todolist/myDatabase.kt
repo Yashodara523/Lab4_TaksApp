3421173146ebb7d5d1473f04a841f55d4f996740
Yashodara523 <nethmini2002yasho@gmail.com>
2024-05-15 15:59:45 +0530

    gy

--- app/src/main/java/com/example/todolist/myDatabase.kt
+++ app/src/main/java/com/example/todolist/myDatabase.kt
@@ -3,6 +3,7 @@ package com.example.todolist
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+//roomDatabase
 
 @Database(entities = [Entity::class], version = 1)
 abstract class myDatabase :RoomDatabase(){
